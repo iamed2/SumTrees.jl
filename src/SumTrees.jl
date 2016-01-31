26e16d3901f11d06d06cc8d886852931e8721e7c
Eric Davies <iamed2@gmail.com>
2016-01-31 11:26:19 -0600

    The codes.

--- src/SumTrees.jl
+++ src/SumTrees.jl
@@ -1,5 +1,75 @@
 module SumTrees
 
-# package code goes here
+export SumTree
+
+import Base: show, setindex!, getindex, sum
+import Base: start, next, done, length, eltype
+
+immutable SumTree{T, NCells}
+    tree::Vector{T}
+end
+
+function SumTree{T}(::Type{T}, ncells::Integer)
+    SumTree{T, ncells}(zeros(T, 2 * ncells - 1))
+end
+
+# default type is Float64
+SumTree(ncells::Integer) = SumTree(Float64, ncells)
+
+function setindex!{T, NCells}(tree::SumTree{T, NCells}, value::T, idx::Integer)
+    if idx < 1 || idx > NCells
+        throw(BoundsError(tree, idx))
+    end
+
+    idx += NCells - 1
+    tree.tree[idx] = value
+
+    # bubble up sums
+    while idx > 1
+        tree.tree[idx >>> 1] = tree.tree[idx] + tree.tree[idx $ 1]
+        idx >>>= 1
+    end
+
+    return nothing
+end
+
+function setindex!{T, NCells}(tree::SumTree{T, NCells}, value, idx::Integer)
+    setindex!(tree, convert(T, value), idx)
+end
+
+function getindex{T, NCells}(tree::SumTree{T, NCells}, idx)
+    if idx < 1 || idx > NCells
+        throw(BoundsError(tree, idx))
+    end
+
+    tree.tree[idx + NCells - 1]
+end
+
+sum(tree::SumTree) = tree.tree[1]
+
+length{T, NCells}(tree::SumTree{T, NCells}) = NCells
+
+start{T, NCells}(tree::SumTree{T, NCells}) = NCells
+next{T, NCells}(tree::SumTree{T, NCells}, state) = (tree.tree[state]::T, state + 1)
+done{T, NCells}(tree::SumTree{T, NCells}, state) = state >= 2 * NCells
+
+eltype{T, NCells}(tree::SumTree{T, NCells}) = T
+
+function show{T, NCells}(io::IO, tree::SumTree{T, NCells})
+    println(io, "SumTree{$T, $(NCells)} ::")
+
+    level = 1
+    while level <= NCells
+        print(io, "    (")
+        next_level = level * 2
+        for i = level:(next_level - 1)
+            print(io, "$(tree.tree[i]),")
+        end
+        println(io, ")")
+
+        level = next_level
+    end
+end
+
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,75 @@
 using SumTrees
 using Base.Test
 
-# write your own tests here
-@test 1 == 1
+@testset "Tiny" begin
+    @testset "Default Type" begin
+        tree = SumTree(1)
+        @test isa(tree, SumTree)
+        @test isa(tree, SumTree{Float64, 1})
+        @test eltype(tree) === Float64
+
+        @test tree[1] == 0.0
+        @test sum(tree) == 0.0
+
+        tree[1] = 3.0
+        @test tree[1] == 3.0
+        @test sum(tree) == 3.0
+
+        tree[1] = 8
+        @test tree[1] == 8.0
+        @test sum(tree) == 8.0
+        @test isa(tree[1], Float64)
+
+        @test_throws BoundsError tree[0]
+        @test_throws BoundsError tree[2]
+
+        @test_throws BoundsError tree[0] = 1.0
+        @test_throws BoundsError tree[2] = 1.0
+
+        @test [x for x in tree] == [8.0]
+    end
+
+    @testset "Rational" begin
+        tree = SumTree(Rational, 1)
+        @test isa(tree, SumTree)
+        @test isa(tree, SumTree{Rational, 1})
+
+        @test tree[1] == zero(Rational)
+        @test sum(tree) == zero(Rational)
+
+        tree[1] = 3//4
+        @test tree[1] == 3//4
+        @test sum(tree) == 3//4
+        @test isa(tree[1], Rational)
+
+        @test [x for x in tree] == [3//4]
+    end
+end
+
+@testset "Normal" begin
+    @testset "Default Type" begin
+        tree = SumTree(8)
+
+        @test collect(tree) == zeros(Float64, 8)
+
+        buffer = IOBuffer()
+        show(buffer, tree)
+
+        @test takebuf_string(buffer) == "SumTree{Float64, 8} ::\n    (0.0,)\n    (0.0,0.0,)\n    (0.0,0.0,0.0,0.0,)\n    (0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,)\n"
+
+        tree[1] = 3
+        tree[4] = 99
+
+        @test tree[1] == 3
+        @test tree[4] == 99
+        @test tree[2] == 0
+        @test sum(tree) == 102
+
+        tree[8] = 0.0001
+        @test tree[8] == 0.0001
+        @test tree[1] == 3
+        @test tree[4] == 99
+        @test tree[2] == 0
+        @test sum(tree) == 102.0001
+    end
+end
